7f3dee6ecfbac2a0cb3ddfe2a0525d5371340221
Marko Srbakov <85948841+markosrbakov@users.noreply.github.com>
2025-02-20 02:20:06 +0100

    Update urls.py

--- GalacticCaffe/urls.py
+++ GalacticCaffe/urls.py
@@ -4,12 +4,15 @@ from GalacticMenu import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from GalacticMenu.views import quote_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),  # Патека за главната страница
     path('category/<str:category_name>/', views.product_by_category, name='product_by_category'),  # Патека за категорија
     path('events/', views.events_view, name='events'),
     path('gallery/', views.gallery_view, name='gallery'),
+    path("quote/", quote_view, name="quote"),
 ]
 
 
